f512e97647cad319a73693387b53a4235402cc5e
Laksamana Adhito <lzadhito@gmail.com>
2022-06-19 13:56:10 +0700

    feat: remove preact build which cause some bugs & inconsistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
-  <h1 className="text-3xl font-bold text-blue-400 underline">
-    Hello world
-  </h1>
+  <h1 className="text-3xl font-bold text-blue-400 underline">Hello world</h1>
 );
 
 export default Home;
